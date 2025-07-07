e13dcf942cbf354e2cf5a03e182afaff74b8bf73
KOHEI <ko.08financier.2mitchan4@gmail.com>
2025-07-08 00:01:22 +0900

    0707学習分

--- tessoku_book/a07.rb
+++ tessoku_book/a07.rb
@@ -1,14 +1,14 @@
 d = gets.to_i
 n = gets.to_i
-b = Array.new(n, 0)
-n.times {
+b = Array.new(d+1, 0)
+n.times do
   l, r = gets.split.map(&:to_i)
   # 出席する日を＋１する。
-  b[l] += 1
-  b[r+1] -= 1
-}
+  b[l-1] += 1
+  b[r] -= 1
+end
 res = 0
 (0...d).each { |i|
-  res = b[i]
+  res += b[i]
   puts res
 }
