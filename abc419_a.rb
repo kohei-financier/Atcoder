1f66617ae9c82743b5bb1cffe1de7d471fe9961e
KOHEI <ko.08financier.2mitchan4@gmail.com>
2025-08-19 23:55:43 +0900

    0816コンテスト分

---
+++ abc419_a.rb
@@ -0,0 +1,12 @@
+# 問題
+# https://atcoder.jp/contests/abc419/tasks/abc419_a
+s = gets.chomp
+if s == "red"
+  puts "SSS"
+elsif s == "blue"
+  puts "FFF"
+elsif s == "green"
+  puts "MMM"
+else
+  puts "Unknown"
+end
