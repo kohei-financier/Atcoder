de14e1dbc15204fd185f90c03d71aedf90230e33
KOHEI <ko.08financier.2mitchan4@gmail.com>
2025-10-08 23:36:12 +0900

    1008学習分

--- abc393_a.rb
+++ abc393_a.rb
@@ -1,5 +1,17 @@
 # 問題
 # https://atcoder.jp/contests/abc393/tasks/abc393_a
+takahashi, aoki = gets.split(" ").map(&:to_s)
+if takahashi == "sick" && aoki == "fine"
+  puts 2
+elsif takahashi == "fine" && aoki == "fine"
+  puts 4
+elsif takahashi == "fine" && aoki == "sick"
+  puts 3
+elsif takahashi == "sick" && aoki == "sick"
+  puts 1
+end
+
+
 s1, s2 = gets.split
 case s1
 when 'fine'
